f8b19277230a4978a0ad687e10a01c8e9a60271e
marymonty <montgm2@rpi.edu>
2019-03-27 23:42:46 -0400

    comments

--- srt.py
+++ srt.py
@@ -3,14 +3,22 @@ import scheduler
 import logger
 import math
 
-
+'''
+	fillReadyQueue
+	fills the ready queue based on priority (tau)
+	params: 
+		p_scheduler: the process scheduler
+		global_time: the current time in the simulation
+		l: lambda
+		alpha: alpha used for tau calculations
+		context_switch_time: the time it takes for a full context switch
+	returns an updated global_time
+'''
 def fillReadyQueue(p_scheduler, global_time, l, alpha, context_switch_time):
-	#print("\tIN FILLREADYQUEUE")
 	#check if there are no more processes
 	if not p_scheduler.processes.empty():
-		#print("\tthere are more processes")
+		#there are more processes
 		while not p_scheduler.processes.empty() and p_scheduler.processes.queue[0][2].arrival_time <= global_time:
-			#print("\tTHE GLOBAL TIME IS %d AND PROCESS %d ARRIVES AT %d" %(global_time, p_scheduler.processes.queue[0][2].pid, p_scheduler.processes.queue[0][2].arrival_time))
 			#this get removes this node from p_scheduler.processes
 			current_node = p_scheduler.processes.get()
 			#put is a tuple, where (1,2,3) 1 is how the queue is ordered, 2 is the tie breaker
@@ -19,39 +27,63 @@ def fillReadyQueue(p_scheduler, global_time, l, alpha, context_switch_time):
 			#current_node[2] = Process object
 			if (current_node[2].tau == 0):
 				current_node[2].tau = math.ceil(1/l)
-				#print("\t FIRST TAU = %f" %current_node[2].tau)
 			else:
 				#tau(i+1) = (alpha * t(i)) + ((1-alpha) * tau(i))
 				current_node[2].tau = math.ceil((alpha * current_node[2].cpu_burst_times[current_node[2].curr_cpu_burst]) + ((1-alpha) * current_node[2].tau))
-				#print("\t NEW TAU = %f" %current_node[2].tau)
+			#put the process in the ready queue by tau priority
 			p_scheduler.ready_queue.put((current_node[2].tau, current_node[1], current_node[2]))
 			r_q = p_scheduler.returnPrintableReadyQueue()
 			print("time %dms: Process %c (tau %dms) arrived; added to ready queue [Q %s]" %(global_time, current_node[1], current_node[2].tau, r_q))
-			#global_time = runContextSwitch(p_scheduler, global_time, context_switch_time, alpha)
 	return global_time
 
 
+'''
+	incrementWaitTime
+	increments the wait time in the logger (used for final calculations)
+	params:
+		p_scheduler: the process scheduler
+'''
 def incrementWaitTime(p_scheduler):
 	if not p_scheduler.ready_queue.empty():
 		for proc in p_scheduler.ready_queue.queue:
 			p_scheduler.logger[proc[2].pid].wait_time += 1
-			#print("    INCREMENTING WAIT TIME")
 
 
-def runContextSwitch(p_scheduler, global_time, context_switch_time, alpha, l, time_change):
-	#print("\tPerforming context switch at time %d" %global_time)
+'''
+	runContextSwitch
+	runs the context switch
+	during a context switch we should: 
+		fill the ready queue with any processes that arrive
+		continue to run the i/o bursts in the blocking queue
+		increment wait time
+		increment global time
+	params:
+		p_scheduler: the process scheduler
+		global_time: the time in the simulation
+		context_switch_time: the time it takes for one full context switch
+		alpha: alpha, needed for tau calculations
+		l: lambda
+	returns:
+		the updated global time, and True (true that there was a context switch)
+'''
+def runContextSwitch(p_scheduler, global_time, context_switch_time, alpha, l):
+	#get the running process
 	run_proc = p_scheduler.running
+	#and set it to none so that there are no preemptions while running a context switch
 	p_scheduler.running = None
 	cs_time = 0
+	#only run half of the context switch time when loading process into running or taking it out
 	while cs_time < .5*context_switch_time:
-		#increment io time and blocking things
+		#can do ready queue things
 		fillReadyQueue(p_scheduler, global_time, l, alpha, context_switch_time)
-		checkBlockingQueue(p_scheduler, global_time, alpha, context_switch_time, l, time_change)
+		#increment io time and blocking things
+		checkBlockingQueue(p_scheduler, global_time, alpha, context_switch_time, l)
+		#increment global time (and wait time?)
 		global_time += 1
-		#print("\t time: %dms" %global_time)
 		#incrementWaitTime(p_scheduler)
 		cs_time += 1
 	return global_time, True
+
 
 """
  readyQueueToRunning
@@ -65,65 +97,63 @@ def runContextSwitch(p_scheduler, global_time, context_switch_time, alpha, l, ti
                     if the remaining time is less than the running process remaining time:
                         there is a preemption, current running process gets sent back to readyQueue, reset the running process with new process
                     else the new remaining time is greater than the running process remaining time:
-                        let the current running process keep running  
+                        let the current running process keep running 
+    params:
+    	p_scheduler: process scheduler
+    	alpha: alpha used for tau
+    	global_time: the global time in the simulation
+    	context_switch_time: the time it takes for a full context switch
+    	l: lambda used for tau
+    return:
+    	updated global time, and if there was a context switch (True or False) 
 """
-def readyQueueToRunning(p_scheduler, alpha, global_time, context_switch_time, l, time_change):
-	#print("\tIN READYQUEUETORUNNING")
-	#print("     readyQueuetoRUnning global time: %d" %global_time)
+def readyQueueToRunning(p_scheduler, alpha, global_time, context_switch_time, l):
+	#if_switched is a bool to tell if there was a context switch
 	if_switched = False
 	if not p_scheduler.ready_queue.empty():
-		#print("\tthe ready queue is not empty")
+		#the ready queue is not empty
 		if p_scheduler.running is None:
 			#there is no process currently running
-			#print("\tthere is no process currently running")
 			current_node = p_scheduler.ready_queue.get()
 			#set the remaining time of the current node if not already set
 			if (current_node[2].remaining_time == -1):
 				current_node[2].remaining_time = current_node[2].cpu_burst_times[current_node[2].curr_cpu_burst]
-			#p_scheduler.running = current_node[2]
 			r_q = p_scheduler.returnPrintableReadyQueue()
-			global_time, if_switched = runContextSwitch(p_scheduler, global_time, context_switch_time, alpha, l, time_change)
+			#perform the context switch to load the process into running
+			global_time, if_switched = runContextSwitch(p_scheduler, global_time, context_switch_time, alpha, l)
+			#set the running process
 			p_scheduler.running = current_node[2]
+			#increment context switch count
 			p_scheduler.logger[p_scheduler.running.pid].num_context_switches += 1
+			#the print for if the process is running an already started burst time
 			if (p_scheduler.running.remaining_time < p_scheduler.running.cpu_burst_times[p_scheduler.running.curr_cpu_burst]):
 				print("time %dms: Process %c started using the CPU with %dms remaining [Q %s]" %(global_time, p_scheduler.running.pid, p_scheduler.running.remaining_time, r_q))
 				p_scheduler.running.time_finished = global_time + p_scheduler.running.remaining_time
-				#print("  time finished : %d" %p_scheduler.running.time_finished)
+			#the print for if the process is running a new burst time
 			else:
 				print("time %dms: Process %c started using the CPU for %dms burst [Q %s]" %(global_time, p_scheduler.running.pid, p_scheduler.running.cpu_burst_times[p_scheduler.running.curr_cpu_burst], r_q))
 				p_scheduler.running.time_finished = global_time + p_scheduler.running.cpu_burst_times[p_scheduler.running.curr_cpu_burst]
-				#print("  time finished : %d" %p_scheduler.running.time_finished)
 			p_scheduler.running.remaining_time += 1
 		else:
 			#there is currently a process running
-			#print("\tthere is currently a process running")
-			#print("running process is: %d" %p_scheduler.running.pid)
 			current_node = p_scheduler.ready_queue.get()
 			#check the new node's remaining time against the currently running process remaining time
-			#print("               current process remaining time: %d " %current_node[2].remaining_time)
-			#print("                     running process remaining time: %d" %p_scheduler.running.remaining_time)
 			if (current_node[2].remaining_time == -1):
 				current_node[2].remaining_time = current_node[2].cpu_burst_times[current_node[2].curr_cpu_burst]
-			#print("\tnew process remaining time: %d\n\tcurrent running process remaining time: %d" %(current_node[2].remaining_time, p_scheduler.running.remaining_time))
-			#if (current_node[2].remaining_time < p_scheduler.running.remaining_time):
+			#calculate the running process's new priority (but do not reset its tau)
 			new_priority = p_scheduler.running.tau - (p_scheduler.running.cpu_burst_times[p_scheduler.running.curr_cpu_burst] - p_scheduler.running.remaining_time)
 			if(current_node[2].tau < new_priority):
-				#print("                                   \n\nfound preemption here\n\n")
 				#there is a preemption
 				r_q = p_scheduler.returnPrintableReadyQueue()
 				print("time %dms: Process %c (tau %d) will preempt %c [Q %s]" %(global_time, current_node[2].pid, current_node[2].tau, p_scheduler.running.pid, r_q))
-
-				#print("\tPREEMPTION")
+				#increment preemption
 				p_scheduler.logger[p_scheduler.running.pid].num_premptions += 1
-				
+				#put the process into the ready queue
 				p_scheduler.ready_queue.put((new_priority, p_scheduler.running.pid, p_scheduler.running))
-				#once for removing process in there
-				global_time, if_switched = runContextSwitch(p_scheduler, global_time, context_switch_time, alpha, l, time_change)
-				#print("          Global time 1: %d " %global_time)
-				#once for putting new process in
-				global_time, if_switched = runContextSwitch(p_scheduler, global_time, context_switch_time, alpha, l, time_change)
-				#print("          Global time 2: %d " %global_time)
-				#print("                GLOBAL TIME IN READY TO RUNNING: %d" %global_time)
+				#context switch once for removing process in there
+				global_time, if_switched = runContextSwitch(p_scheduler, global_time, context_switch_time, alpha, l)
+				#context switch again for putting new process in
+				global_time, if_switched = runContextSwitch(p_scheduler, global_time, context_switch_time, alpha, l)
 				#need to send currently running process back to the ready queue
 				#dont actually recalculate tau, just update the priority
 				p_scheduler.running = current_node[2]
@@ -132,31 +162,38 @@ def readyQueueToRunning(p_scheduler, alpha, global_time, context_switch_time, l,
 				r_q = p_scheduler.returnPrintableReadyQueue()
 				print("time %dms: Process %c started using the CPU for %dms burst [Q %s]" %(global_time, p_scheduler.running.pid, p_scheduler.running.cpu_burst_times[p_scheduler.running.curr_cpu_burst], r_q))
 				p_scheduler.running.time_finished = global_time + p_scheduler.running.cpu_burst_times[p_scheduler.running.curr_cpu_burst]
-				#print("  time finished : %d" %p_scheduler.running.time_finished)
 				p_scheduler.running.remaining_time += 1
 			else:
 				#there was no preemption
-				#print("\tNO PREEMPTION")
 				p_scheduler.ready_queue.put((current_node[2].tau, current_node[2].pid, current_node[2]))
-				#p_scheduler.running.remaining_time -= 1
 	return global_time, if_switched
 
 
 
-def decrementRunning(p_scheduler, global_time, alpha, context_switch_time, l, time_change):
-	#print("\t     IN DECREMENTRUNNING")
-	#print("           decrementRunning global time %d" %global_time)
+'''
+	decrementRunning
+	a function that decrements the count of the running process
+	also checks if it is completed its cpu burst
+	and if so will move it to blocking to begin its io bursts
+	params:
+		p_scheduler: process scheduler
+    	global_time: the global time in the simulation
+    	alpha: alpha used for tau
+    	context_switch_time: the time it takes for a full context switch
+    	l: lambda used for tau
+    returns:
+    	updated global time, and if there was a context switch (True or False) 
+'''
+def decrementRunning(p_scheduler, global_time, alpha, context_switch_time, l):
+	#variable for if there was a context switch initally set to false
 	if_switched = False
+
 	if not p_scheduler.running is None:
 		p_scheduler.running.remaining_time -= 1
-		#print("                                          remaining time: %d" %p_scheduler.running.remaining_time)
 		p_scheduler.logger[p_scheduler.running.pid].cpu_time += 1
 		#process is leaving running
 		#if (p_scheduler.running.remaining_time == -1):
 		if (global_time == p_scheduler.running.time_finished):
-
-			#global_time = runContextSwitch(p_scheduler, global_time, context_switch_time, alpha)
-			#print("                   GLOBAL TIME 1: %d" %global_time)
 			#increment the current cpu burst
 			p_scheduler.running.curr_cpu_burst += 1
 			if (p_scheduler.running.curr_cpu_burst != p_scheduler.running.num_bursts):
@@ -183,7 +220,7 @@ def decrementRunning(p_scheduler, global_time, alpha, context_switch_time, l, ti
 				print("time %dms: Process %c switching out of CPU; will block on I/O unitl time %dms [Q %s]" %(global_time, p_scheduler.running.pid, (p_scheduler.running.io_burst_times[p_scheduler.running.curr_io_burst] + global_time + .5*context_switch_time), r_q))
 				p_scheduler.running.time_finished = (p_scheduler.running.io_burst_times[p_scheduler.running.curr_io_burst] + global_time + .5*context_switch_time)
 				run_proc = p_scheduler.running
-				global_time, if_switched = runContextSwitch(p_scheduler, global_time, context_switch_time, alpha, l, time_change)
+				global_time, if_switched = runContextSwitch(p_scheduler, global_time, context_switch_time, alpha, l)
 				p_scheduler.running = run_proc
 				#print("                  time is %d : IO burst is %d long " %(global_time, p_scheduler.running.io_burst_times[p_scheduler.running.curr_io_burst]))
 				p_scheduler.running.remaining_time += 1
@@ -205,7 +242,7 @@ def decrementRunning(p_scheduler, global_time, alpha, context_switch_time, l, ti
 
 
 
-def checkIO(proc, alpha, p_scheduler, global_time, context_switch_time, l, time_change):
+def checkIO(proc, alpha, p_scheduler, global_time, context_switch_time, l):
 	preempt = 0
 	flagged = False
 	#check if the time is finished time
@@ -248,7 +285,7 @@ def checkIO(proc, alpha, p_scheduler, global_time, context_switch_time, l, time_
 
 
 
-def checkBlockingQueue(p_scheduler, global_time, alpha, context_switch_time, l, time_change):
+def checkBlockingQueue(p_scheduler, global_time, alpha, context_switch_time, l):
 	preempt = 0
 	if  len(p_scheduler.blocking) > 0:
 			#the blocking array is not empty
@@ -257,7 +294,7 @@ def checkBlockingQueue(p_scheduler, global_time, alpha, context_switch_time, l, 
 			for proc in p_scheduler.blocking:
 				#this function decrememnts the remaining time
 				#also moves any finished processes back to the ready queue
-				preempt, global_time = checkIO(proc, alpha, p_scheduler, global_time, context_switch_time, l, time_change)
+				preempt, global_time = checkIO(proc, alpha, p_scheduler, global_time, context_switch_time, l)
 				#preemtpt == 1 there was a preemption
 				if preempt == 1:
 					return preempt, global_time
@@ -301,10 +338,10 @@ def runSRT(processes, num_processes, context_switch_time, alpha, l):
 	p_scheduler = scheduler.Scheduler(processes)
 	#print the arrivals of the p_scheduler
 	p_scheduler.printArrivals()
-	#for decrementing purposes
-	time_change = 0
+	#for decrementing purposes = 0
 	#print the simulation start
 	r_q = p_scheduler.returnPrintableReadyQueue()
+	print(p_scheduler)
 	print("time %dms: Simulator started for SRT [Q %s]" %(global_time, r_q))
 
 	cpu_sum = 0
@@ -326,23 +363,21 @@ def runSRT(processes, num_processes, context_switch_time, alpha, l):
 		#global_time = decrementRunning(p_scheduler, global_time, alpha, context_switch_time, l)
 		#if (p_scheduler.running != None):
 		#	print("     after decrement running: TIME %d, process %c remaining time is %d" % (global_time, p_scheduler.running.pid, p_scheduler.running.remaining_time))
-		global_time, if_switched1 = readyQueueToRunning(p_scheduler, alpha, global_time, context_switch_time, l, time_change)
+		global_time, if_switched1 = readyQueueToRunning(p_scheduler, alpha, global_time, context_switch_time, l)
 		#print("  if switched 1 = %s" %if_switched1)
-		global_time, if_switched2 = decrementRunning(p_scheduler, global_time, alpha, context_switch_time, l, time_change)
+		global_time, if_switched2 = decrementRunning(p_scheduler, global_time, alpha, context_switch_time, l)
 		#print("  if switched 2 = %s" %if_switched2)
 		#global_time = checkBlockingQueue(p_scheduler, global_time, alpha, context_switch_time, l)
 		
 		#instead of a separate function, gonna check the blocking queue here so i can
 		#better deal with a preemption
 		if_switched3 = False
-		preempt, global_time = checkBlockingQueue(p_scheduler, global_time, alpha, context_switch_time, l, time_change)
+		preempt, global_time = checkBlockingQueue(p_scheduler, global_time, alpha, context_switch_time, l)
 		if (preempt == 1):
 			#global_time -= 1
 			#gonna try a context switch here for removing the current running process
 			run_proc = p_scheduler.running
-			global_time, if_switched3 = runContextSwitch(p_scheduler, global_time, context_switch_time, alpha, l, time_change)
-			#print("            global time after preempt context switch: %d" %global_time)
-			#p_scheduler.running = ru
+			global_time, if_switched3 = runContextSwitch(p_scheduler, global_time, context_switch_time, alpha, l)
 		
 
 		if (p_scheduler.running != None):
@@ -352,9 +387,6 @@ def runSRT(processes, num_processes, context_switch_time, alpha, l):
 		#if there was any context switch we dont want to increment the time
 		if (not if_switched1 and not if_switched2 and not if_switched3):
 			global_time += 1
-			time_change = 0
-		else:
-			time_change += 1
 		if p_scheduler.processes.empty() and p_scheduler.ready_queue.empty() and (p_scheduler.running == None) and (len(p_scheduler.blocking) == 0):
 			r_q = p_scheduler.returnPrintableReadyQueue()
 			print("time %dms: Simulator ended for SRT [Q %s]" %(global_time, r_q))
